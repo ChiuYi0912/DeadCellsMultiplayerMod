cae3c4e19372df1baca1b5ecae465e3f30f0e4e7
ChiuYi <3360625360@qq.com>
2026-01-16 13:03:29 +0800

    update Minimap

--- UI/Minimapreveal.cs
+++ UI/Minimapreveal.cs
@@ -8,6 +8,7 @@ using dc.ui;
 using dc.ui.hud;
 using dc.ui.hud.map;
 using HaxeProxy.Runtime;
+using ModCore.Utitities;
 using Debug = Serilog.Log;
 
 namespace DeadCellsMultiplayerMod.Minimap
@@ -24,21 +25,31 @@ namespace DeadCellsMultiplayerMod.Minimap
             Hook_MiniMap.postUpdate += Hook_MiniMap_postUpdate;
             Hook_MiniMap.initContainers += Hook_MiniMap_initContainers;
         }
-
+        private Dictionary<int, Graphics> _kingMarkers = new Dictionary<int, Graphics>();
         private void Hook_MiniMap_initContainers(Hook_MiniMap.orig_initContainers orig, MiniMap self, Bytes max)
         {
             orig(self, max);
+            int length = ModEntry.clients.Length;
 
-            bool create = true;
-            this.kingcircle = self.fow.createGraphics(Ref<bool>.From(ref create));
-            var kmap = this.kingcircle;
-            int color = 16711680;
-            double alpha = 0;
-            kmap.beginFill(Ref<int>.From(ref color), Ref<double>.From(ref alpha));
-            kmap.drawCircle(0.0, 0.0, 17.0, Ref<int>.Null);
-            kmap.endFill();
-            kmap.set_visible(true);
-            kmap.posChanged = true;
+            for (int i = 0; i < length; i++)
+            {
+                var displayName = ModEntry.GetClientLabel(i);
+                dc.String remoteUsername = displayName.AsHaxeString();
+
+                bool create = true;
+                this.kingcircle = self.fow.createGraphics(Ref<bool>.From(ref create));
+                var kmap = this.kingcircle;
+                int color = 16711680;
+                double alpha = 1;
+                kmap.beginFill(Ref<int>.From(ref color), Ref<double>.From(ref alpha));
+                kmap.drawCircle(0.0, 0.0, 17.0, Ref<int>.Null);
+                kmap.endFill();
+                kmap.set_visible(true);
+                kmap.posChanged = true;
+
+                _kingMarkers[i] = kmap;
+            }
+
 
         }
 
@@ -59,8 +70,19 @@ namespace DeadCellsMultiplayerMod.Minimap
                     mmtracker = arrayElement;
                     if (king != null && mmtracker != null && Std.Class.@is(mmtracker.e, KingSkin.Class))
                     {
-                        kmap.x = king.cx;
-                        kmap.y = king.cy;
+                        var clients = ModEntry.clients;
+                        for (int k = 0; k < clients.Length; k++)
+                        {
+                            var client = clients[k];
+
+                            if (client == null) continue;
+
+                            if (!_kingMarkers.TryGetValue(k, out var marker))
+                                continue;
+
+                            marker.x = client.cx;
+                            marker.y = client.cy;
+                        }
                     }
                 }
 
